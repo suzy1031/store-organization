d1bfe951b6fefb875c3fd08feb1ce6bdeb35b486
kohei-sugawara <89954502+kohei-sugawara@users.noreply.github.com>
2021-11-05 00:22:15 +0900

    add function

--- src/libs/LevelColor.js
+++ src/libs/LevelColor.js
@@ -19,3 +19,29 @@ export const levelColorObject = (level) => {
     return { background: '#FF6666' };
   }
 };
+
+export const LawnColor = (time, member) => {
+  if (1 <= time && time < 10 && member <= 1) {
+    return {
+      color: '#fff',
+      background: '#00FF00',
+    };
+  } else if (10 <= time && time < 60 && 3 <= member && member <= 5) {
+    return {
+      color: '#fff',
+      background: '#00CC00',
+    };
+  } else if (60 < time && 5 <= member) {
+    return {
+      color: '#fff',
+      background: '#004400',
+    };
+  } else if (time === 0 && member === 0) {
+    return {};
+  } else {
+    return {
+      color: '#fff',
+      background: '#007700',
+    };
+  }
+};
